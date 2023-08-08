f995793b3c8f81bf8ad029cdb6d661d8db126530
Jordi Calafat <j0rdi@Jordis-MacBook-Pro.local>
2023-08-08 21:26:06 +0200

    improve types

--- src/framework/types.ts
+++ src/framework/types.ts
@@ -2,7 +2,10 @@ import { type RequestHandler } from 'express'
 import { type Model } from 'mongoose'
 import { type AnyZodObject } from 'zod'
 
-export interface CommonHandlers<T> {
+export type CommonMethod = keyof CommonHandlers
+export type HTTPMethod = 'get' | 'post' | 'put' | 'patch' | 'delete'
+
+export interface CommonHandlers<T = any> {
   list?: RequestHandler | RequestHandler[]
   create?: RequestHandler<{}, {}, T> | RequestHandler<{}, {}, T>[]
   read?: RequestHandler<{ id: string }> | RequestHandler<{ id: string }>[]
@@ -11,9 +14,6 @@ export interface CommonHandlers<T> {
     | RequestHandler<{ id: string }, {}, Partial<T>>[]
   delete?: RequestHandler<{ id: string }> | RequestHandler<{ id: string }>[]
 }
-
-export type CommonMethod = 'list' | 'create' | 'read' | 'update' | 'delete'
-export type HTTPMethod = 'get' | 'post' | 'put' | 'patch' | 'delete'
 
 export type CustomHandlers = Record<
   string,
