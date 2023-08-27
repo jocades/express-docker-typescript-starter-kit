0e5411729fa03d65fc53e62039871ba367bf6214
Jordi Calafat <jcalafat97@gmail.com>
2023-08-27 22:09:00 +0200

    clenup + add members arr to groups migration

--- migrations/20221213041915-groups.js
+++ migrations/20221213041915-groups.js
@@ -5,6 +5,7 @@ module.exports = {
         name: 'Group 1',
         description: 'Group 1 description',
         privacy: 'public',
+        members: [],
         // longitute, latitude of palma de mallorca
         location: { type: 'Point', coordinates: [2.6496, 39.5696] },
         createdAt: new Date(),
@@ -14,6 +15,7 @@ module.exports = {
         name: 'Group 2',
         description: 'Group 2 description',
         privacy: 'private',
+        members: [],
         // longitute, latitude of andratx, mallorca
         location: { type: 'Point', coordinates: [2.4933, 39.6017] },
         createdAt: new Date(),
@@ -22,8 +24,8 @@ module.exports = {
       {
         name: 'Group 3',
         description: 'Group 3 description',
-        privacy: 0,
         privacy: 'public',
+        members: [],
         // longitute, latitude of pollenca, mallorca
         location: { type: 'Point', coordinates: [3.0375, 39.8561] },
         createdAt: new Date(),
@@ -33,6 +35,8 @@ module.exports = {
         name: 'Group 4',
         description: 'Group 4 description',
         privacy: 'private',
+        members: [],
+        // longitute, latitude of pollenca, mallorca
         // longitute, latitude of inca, mallorca
         location: { type: 'Point', coordinates: [2.8197, 39.5696] },
         createdAt: new Date(),
@@ -41,6 +45,8 @@ module.exports = {
       {
         name: 'Group 5',
         description: 'Group 5 description',
+        members: [],
+        // longitute, latitude of pollenca, mallorca
         // longitute, latitude of sa pobla, mallorca
         location: { type: 'Point', coordinates: [2.7333, 39.7333] },
         privacy: 'public',
